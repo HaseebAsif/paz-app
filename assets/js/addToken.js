8c3746852e8988b8d4dce89df35b61603d09404e
shaharyar saleem <shaharyarsaleem71@gmail.com>
2021-08-26 00:07:47 +0500

    fix: requested changes done
    1- Comment on contract address
    2- pazz-coin logo with live pazz website address

--- assets/js/addToken.js
+++ assets/js/addToken.js
@@ -1,7 +1,7 @@
-const tokenAddress = '0x68590a47578E5060a29fd99654f4556dBfa05D10';
+const tokenAddress = '0x68590a47578E5060a29fd99654f4556dBfa05D10'; // This is not the right contract
 const tokenSymbol = 'PAZ';
 const tokenDecimals = 9;
-const tokenImage = 'https://develop.dx27hd910sf2y.amplifyapp.com/assets/img/pazzive-coin-dark.png';
+const tokenImage = 'https://paz.fuzion.team/assets/img/pazzive-coin-dark.png';
 
 async function addToken(){
 const wasAdded = await ethereum.request({
